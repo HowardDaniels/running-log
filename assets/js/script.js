4e1ef1b5819e4e90623fcbea39b1ed0928982579
Howard Daniels <hdaniels13tat@gmail.com>
2020-06-17 23:40:47 -0400

    simplified geocoder with .toString(4)

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,6 @@ function getLocation() {
 }
 
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
-  console.log(position);
-
   
 var latitude = position.coords.latitude;
 var longitude = position.coords.longitude;
@@ -22,14 +18,66 @@ console.log(newLat);
 var newLong = Math.floor(((longitude + 180)/360)*(4**10));
 console.log(newLong);
 
-var arrexp9Lat = [];
+var newLatQuad = newLat.toString(4);
+console.log(newLatQuad)
+var newLongQuad = newLong.toString(4);
+console.log(newLongQuad);
 
-for (i = 0; i < 4; i++){
-  console.log((4**9)*i);
-  if (((4**9)*i) >= newLat){
-    arrexp9Lat.push(i);
+var geocode = "";
+
+for (i = 0; i < 10; i++){
+  if (newLatQuad.charAt(i) == "0" && newLongQuad.charAt(i) == "0"){
+    geocode += "C";
+  }
+  else if (newLatQuad.charAt(i) == "0" && newLongQuad.charAt(i) == "1"){
+    geocode += "D";
+  }
+  else if (newLatQuad.charAt(i) == "0" && newLongQuad.charAt(i) == "2"){
+    geocode += "E";
+  }
+  else if (newLatQuad.charAt(i) == "0" && newLongQuad.charAt(i) == "3"){
+    geocode += "F";
+  }
+  else if (newLatQuad.charAt(i) == "1" && newLongQuad.charAt(i) == "0"){
+    geocode += "8";
+  }
+  else if (newLatQuad.charAt(i) == "1" && newLongQuad.charAt(i) == "1"){
+    geocode += "9";
+  }
+  else if (newLatQuad.charAt(i) == "1" && newLongQuad.charAt(i) == "2"){
+    geocode += "A";
+  }
+  else if (newLatQuad.charAt(i) == "1" && newLongQuad.charAt(i) == "3"){
+    geocode += "B";
+  }
+  else if (newLatQuad.charAt(i) == "2" && newLongQuad.charAt(i) == "0"){
+    geocode += "4";
+  }
+  else if (newLatQuad.charAt(i) == "2" && newLongQuad.charAt(i) == "1"){
+    geocode += "5";
+  }
+  else if (newLatQuad.charAt(i) == "2" && newLongQuad.charAt(i) == "2"){
+    geocode += "6";
+  }
+  else if (newLatQuad.charAt(i) == "2" && newLongQuad.charAt(i) == "3"){
+    geocode += "7";
+  }
+  else if (newLatQuad.charAt(i) == "3" && newLongQuad.charAt(i) == "0"){
+    geocode += "0";
+  }
+  else if (newLatQuad.charAt(i) == "3" && newLongQuad.charAt(i) == "1"){
+    geocode += "1";
+  }
+  else if (newLatQuad.charAt(i) == "3" && newLongQuad.charAt(i) == "2"){
+    geocode += "2";
+  }
+  else if (newLatQuad.charAt(i) == "3" && newLongQuad.charAt(i) == "3"){
+    geocode += "3";
   }
 }
 
-console.log(arrexp9Lat[0]);
+console.log(geocode);
+x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude + 
+  "<br>Geocode: " + geocode;
 }
