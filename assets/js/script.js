2d92cd8d88c6c490f13dfaef364d202e7a72090e
Howard Daniels <hdaniels13tat@gmail.com>
2020-06-21 23:59:24 -0400

    added space

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// function changeFunc() {
+
 var x = document.getElementById("coords");
 
 function getLocation() {
@@ -81,3 +83,117 @@ x.innerHTML = "Latitude: " + position.coords.latitude +
   "<br>Longitude: " + position.coords.longitude + 
   "<br>Geocode: " + geocode;
 }
+
+var selection = document.getElementById("converter");
+selection.value = "coordstogeocode";
+// var selectedValue = selection.value;
+var result = document.getElementById("selectedConverter");
+result.innerHTML = "<p>Latitude: </p> <input>" + "<br><p>Longitude: </p> <input>";
+
+
+function changeFunc() {
+  var selectedValue = selection.options[selection.selectedIndex].value;
+  selection.value = selectedValue;
+
+
+console.log(selection.value);
+
+if (selectedValue == "coordstogeocode"){
+  result.innerHTML = `<p>Latitude: </p> <input id="latitudeValue"> <br><p>Longitude: </p> <input id="longitudeValue">`;
+}
+
+else{
+  result.innerHTML = "<p>Geocode: </p> <input>";
+}
+// }
+
+function coordstogeocode(){
+var latitudeValue = document.getElementById("latitudeValue").value;
+var longitudeValue = document.getElementById("longitudeValue").value;
+
+var lat = Number(latitudeValue);
+var long = Number(longitudeValue);
+
+console.log(latitudeValue + " " + longitudeValue);
+console.log(latitudeValue += longitudeValue);
+console.log(lat + long);
+
+var newLat2 = Math.floor(((lat + 90)/180)*(4**10));
+console.log(newLat2);
+var newLong2 = Math.floor(((long + 180)/360)*(4**10));
+console.log(newLong2);
+
+var newLatQuad2 = newLat2.toString(4);
+console.log(newLatQuad2)
+var newLongQuad2 = newLong2.toString(4);
+console.log(newLongQuad2);
+
+var geocode2 = "";
+
+for (i = 0; i < 10; i++){
+  if (newLatQuad2.charAt(i) == "0" && newLongQuad2.charAt(i) == "0"){
+    geocode2 += "C";
+  }
+  else if (newLatQuad2.charAt(i) == "0" && newLongQuad2.charAt(i) == "1"){
+    geocode2 += "D";
+  }
+  else if (newLatQuad2.charAt(i) == "0" && newLongQuad2.charAt(i) == "2"){
+    geocode2 += "E";
+  }
+  else if (newLatQuad2.charAt(i) == "0" && newLongQuad2.charAt(i) == "3"){
+    geocode2 += "F";
+  }
+  else if (newLatQuad2.charAt(i) == "1" && newLongQuad2.charAt(i) == "0"){
+    geocode2 += "8";
+  }
+  else if (newLatQuad2.charAt(i) == "1" && newLongQuad2.charAt(i) == "1"){
+    geocode2 += "9";
+  }
+  else if (newLatQuad2.charAt(i) == "1" && newLongQuad2.charAt(i) == "2"){
+    geocode2 += "A";
+  }
+  else if (newLatQuad2.charAt(i) == "1" && newLongQuad2.charAt(i) == "3"){
+    geocode2 += "B";
+  }
+  else if (newLatQuad2.charAt(i) == "2" && newLongQuad2.charAt(i) == "0"){
+    geocode2 += "4";
+  }
+  else if (newLatQuad2.charAt(i) == "2" && newLongQuad2.charAt(i) == "1"){
+    geocode2 += "5";
+  }
+  else if (newLatQuad2.charAt(i) == "2" && newLongQuad2.charAt(i) == "2"){
+    geocode2 += "6";
+  }
+  else if (newLatQuad2.charAt(i) == "2" && newLongQuad2.charAt(i) == "3"){
+    geocode2 += "7";
+  }
+  else if (newLatQuad2.charAt(i) == "3" && newLongQuad2.charAt(i) == "0"){
+    geocode2 += "0";
+  }
+  else if (newLatQuad2.charAt(i) == "3" && newLongQuad2.charAt(i) == "1"){
+    geocode2 += "1";
+  }
+  else if (newLatQuad2.charAt(i) == "3" && newLongQuad2.charAt(i) == "2"){
+    geocode2 += "2";
+  }
+  else if (newLatQuad2.charAt(i) == "3" && newLongQuad2.charAt(i) == "3"){
+    geocode2 += "3";
+  }
+}
+
+console.log(geocode2);
+document.getElementById("answer").innerHTML = geocode2;
+}
+
+document.getElementById("submit").addEventListener("click", function(){
+  if (selectedValue == "coordstogeocode"){
+  coordstogeocode();
+  }
+  else if (selectedValue == "geocodetocoords"){
+   console.log("sdssdfsdf")
+    }
+})
+
+}
+
+changeFunc();
